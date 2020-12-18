4f3d1ea8a819b66d14ad5e34113e34045064cf7d
Matthew Kennedy <m.kennedy@me.com>
2020-12-18 23:47:37 +0000

    Update readme

--- lib/spree_shipstation/configuration.rb
+++ lib/spree_shipstation/configuration.rb
@@ -2,6 +2,6 @@
 
 module SpreeShipstation
   class Configuration
-    attr_accessor :username, :password, :weight_units, :ssl_encrypted, :capture_at_notification, :export_canceled_shipments
+    attr_accessor :username, :password, :weight_units, :capture_at_notification, :export_canceled_shipments
   end
 end
