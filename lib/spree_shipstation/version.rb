1ab1d751f086da58912f9adb9f0de96b0f3c3393
Matthew Kennedy <m.kennedy@me.com>
2026-02-21 16:27:07 +0000

    Improve version

--- lib/spree_shipstation/version.rb
+++ lib/spree_shipstation/version.rb
@@ -1,20 +1,9 @@
 # frozen_string_literal: true
 
 module SpreeShipstation
-  module_function
+  VERSION = '2.0.6'.freeze
 
-  # Returns the version of the currently loaded SpreeSimpleSales as a
-  # <tt>Gem::Version</tt>.
-  def version
-    Gem::Version.new VERSION::STRING
-  end
-
-  module VERSION
-    MAJOR = 2
-    MINOR = 0
-    TINY = 6
-    PRE = nil
-
-    STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
+  def self.version
+    Gem::Version.new(VERSION)
   end
 end
